03337f4f48fb6fd660a4b6ffa4a3869f3bca7a58
Rachel Patterson <rachelpatterson16@gmail.com>
2025-03-20 22:20:37 -0700

    adjusted mobile menu slightly

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const dropdown = document.getElementById('dropdown');
 dropdown.style.display = 'none';
 // click event listener to menu toggle
 menuToggle.addEventListener('click', () => {
-    // Toggle the dropdown's visibility
+    // toggle the dropdown's visibility
     if (dropdown.style.display === 'none' || dropdown.style.display === '') {
-        dropdown.style.display = 'block'; // Show the dropdown
+        dropdown.style.display = 'block'; // show the dropdown
     } else {
-        dropdown.style.display = 'none'; // Hide the dropdown
+        dropdown.style.display = 'none'; // hide the dropdown
     }
 });
